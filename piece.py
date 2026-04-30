0ed88c47fc1f149650e0af6f5691b2eaf898387c
Valdack <cp9g2hsk8v@privaterelay.appleid.com>
2026-04-30 08:50:42 +0200

    ecriture init Piece

---
+++ piece.py
@@ -0,0 +1,8 @@
+class Piece:
+    def __init__(self, x, y, clr):
+        self.position = (x, y)
+        self.couleur = clr
+        self.coup_possible = []
+
+    def case_atteignables() -> list:
+        return None
---
+++ pion.py
@@ -0,0 +1,9 @@
+from piece import Piece
+
+
+class Pion(Piece):
+    def __init__(self, x, y, clr):
+        super().__init__(x, y, clr)
+
+    def cases_atteignables():
+        return None
