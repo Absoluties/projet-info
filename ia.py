56c6aeb4580bb43286391e9a8aecdd61d96ea5fc
Aidan <aidan.asd10@hotmail.fr>
2026-06-02 11:55:52 +0200

    ...

--- ia.py
+++ ia.py
@@ -5,20 +5,7 @@ from abc import abstractmethod, ABCMeta
 class IA(ABCMeta):
     def __init__(self, niveau:int, partie:Partie):
         self.partie = partie
-        match niveau:
-            case 0:
-                ...
-            case 1:
-                ...
-            case 2:
-                ...
-            case 3:
-                ...
-            case _:
-                RuntimeError()
-    
-    def minmax():
-        ...
+        self.niveau = niveau
     
     @abstractmethod
     def choisir_coup() -> tuple[tuple[int,int], tuple[int,int]]:
